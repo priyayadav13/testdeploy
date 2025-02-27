d1b93bc5e254cc212d2c97e5c28ce8141f9658ed
Priya Yadav <yadavpriya73028@gmail.com>
2025-02-28 00:31:13 +0530

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
